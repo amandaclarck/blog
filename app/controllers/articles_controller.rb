c6500e4554cf41e1dc0ee89cd28d418f09bccaea
Amanda Pedroso <amanda.lssc@gmail.com>
2018-06-17 20:45:31 -0300

    Projeto blog - Rails Girls 2018

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,63 @@
+	class ArticlesController < ApplicationController
+		def index
+			@articles = Article.all
+		end
+
+		def new 
+			@article = Article.new
+		end
+
+		def edit
+			@article = Article.find(params[:id])
+		end
+=begin
+def create
+	@article = Article.new(params[:article])
+	
+	if @article.save
+		redirect_to root_path, status: :created
+
+	else
+		redirect_to root_path, status: :bad_request
+	end
+end
+=end
+	def show
+		@article = Article.find(params[:id])
+	end
+		
+def create
+  @article = Article.new(article_params)
+ 
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
+ 
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+
+end
